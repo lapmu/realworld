06a4120f55f266a89ee3e9e82c5fd96d68f95eae
lapmu <antjdgkdl@gmail.com>
2023-07-27 16:59:57 +0900

    [chore] 레이아웃 완성

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,27 +1,29 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
   return (
     <>
       <nav className='navbar navbar-light'>
         <div className='container'>
-          <a className='navbar-brand' href='/'>
+          <Link className='navbar-brand' to='/'>
             conduit
-          </a>
+          </Link>
           <ul className='nav navbar-nav pull-xs-right'>
             <li className='nav-item'>
               {/*<!-- Add "active" className when you're on that page" -->*/}
-              <a className='nav-link active' href='/'>
+              <Link className='nav-link active' to='/'>
                 Home
-              </a>
+              </Link>
             </li>
             <li className='nav-item'>
-              <a className='nav-link' href='/login'>
+              <Link className='nav-link' to='/login'>
                 Sign in
-              </a>
+              </Link>
             </li>
             <li className='nav-item'>
-              <a className='nav-link' href='/register'>
+              <Link className='nav-link' to='/register'>
                 Sign up
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
